fe335c846facbe47487eca56ad816395dfd6b4ae
amolu <amolujagare@gmail.com>
2022-08-30 22:04:28 +0530

    Initial commit

---
+++ src/main/java/stepdefinition/Darksky.java
@@ -0,0 +1,17 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Darksky {
+
+    @Given("I am on Darksky Home Page")
+    public void i_am_on_darksky_home_page() {
+
+    }
+    @Then("I verify current temp is equal to Temperature from Daily Timeline")
+    public void i_verify_current_temp_is_equal_to_temperature_from_daily_timeline() {
+
+    }
+
+}
